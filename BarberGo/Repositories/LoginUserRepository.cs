2a0faa85837b6200fd1e35d71f06445154d1ebdc
Themystocles <themystocles@outlook.com>
2025-03-20 09:25:07 -0300

    Finalizando a autenticação com JWT puxando usuario e senha savos no banco. OBS:Ainda falta codificar as permissões dos usuarios

---
+++ BarberGo/Repositories/LoginUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using BarberGo.Data;
+using BarberGo.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BarberGo.Repositories
+{
+    public class LoginUserRepository
+    {
+        private readonly DataContext _context;
+
+        public LoginUserRepository(DataContext context) {
+            _context = context; 
+        }
+
+        public async Task<AppUser?> GetUserByUsernameAsync(string username)
+        {
+            return await _context.AppUsers.FirstOrDefaultAsync(x => x.Email == username);
+        }
+    }
+}
