9d21390ad5639ed77def2c29b4f9af10ce684a42
Themystocles <themystocles@outlook.com>
2025-04-02 10:03:49 -0300

    Sobrescrevendo a propriedade create para tornala publica e permitir o cadastro de novos usuários.

--- BarberGo/Controllers/AppUserController.cs
+++ BarberGo/Controllers/AppUserController.cs
@@ -1,9 +1,11 @@
 ﻿using BarberGo.Entities;
 using BarberGo.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BarberGo.Controllers
 {
+    
     [ApiController]
     [Route("api/[controller]")]
     public class AppUserController : GenericRepositoryController<AppUser>
@@ -21,6 +23,13 @@ namespace BarberGo.Controllers
         public override Task<ActionResult<AppUser>> GetByIdAsync(int id)
         {
             return Task.FromResult<ActionResult<AppUser>>(Unauthorized("Acesso negado. Você não tem permissão para executar esta ação."));
+        }
+        [HttpPost("create")]
+        [AllowAnonymous] // Apenas a criação de usuário é liberada
+        public override async Task<ActionResult<AppUser>> CreateEntity(AppUser entity)
+        {
+            var createdEntity = await _genericRepositoryServices.CreateAsync(entity);
+            return Created("", createdEntity);
         }
 
     }
