113477925b08967d729f405f89b0331a052f7684
Themystocles <themystocles@outlook.com>
2025-05-07 10:11:04 -0300

    Adicionando Corestesagendados

---
+++ BarberGo/Interfaces/ITodaysCustomers.cs
@@ -0,0 +1,10 @@
+﻿using BarberGo.Entities;
+using BarberGo.Entities.DTOs;
+
+namespace BarberGo.Interfaces
+{
+    public interface ITodaysCustomers
+    {
+        Task<List<CustomerOfTheDayDto>> MyCustomersOfTheDay(DateTime date, int BarberId);
+    }
+}
---
+++ BarberGo/Repositories/TodaysCustomers.cs
@@ -0,0 +1,41 @@
+﻿using BarberGo.Data;
+using BarberGo.Entities;
+using BarberGo.Entities.DTOs;
+using BarberGo.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarberGo.Repositories
+{
+    public class TodaysCustomers : ITodaysCustomers
+    {
+        private readonly DataContext _context;
+
+        public TodaysCustomers(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<CustomerOfTheDayDto>> MyCustomersOfTheDay(DateTime date, int barberid)
+        {
+
+            return await _context.Appointments
+             .Include(a => a.Client)
+             .Include(a => a.Haircut)
+             .Where(a => a.BarberId == barberid && a.DateTime.Date == date.Date)
+             .Select(a => new CustomerOfTheDayDto
+        {
+              ClientName = a.Client.Name,
+              ClientPhone = a.Client.Phone,
+              HaircutName = a.Haircut.Name,
+              HaircutPreco = a.Haircut.Preco,
+              DateTime = a.DateTime,
+              Status = a.Status
+         })
+            .ToListAsync();
+         }
+
+
+
+
+    
+    }
+}
