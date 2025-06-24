4afadd85725fc6ab97c9e6907d73628db9ca57f0
Themystocles <themystocles@outlook.com>
2025-06-24 09:56:58 -0300

    Enviando email de onfirmação par ao cliente

--- BarberGo/Services/EmailServices.cs
+++ BarberGo/Services/EmailServices.cs
@@ -46,6 +46,23 @@ namespace BarberGo.Services
 
             await _emailSender.SendEmailAsync(destine, subject, body);
 
+            // E-mail para o CLIENTE
+            string clientEmail = appoint.Client.Email;
+            string subjectToClient = "Seu agendamento foi confirmado!";
+            string bodyToClient = $@"
+        <p>Olá <strong>{appoint.Client.Name}</strong>,</p>
+        <p>Seu agendamento foi confirmado com sucesso! Veja os detalhes abaixo:</p>
+        <ul>
+            <li><strong>Barbeiro:</strong> {appoint.Barber.Name}</li>
+            <li><strong>Corte:</strong> {appoint.Haircut.Name}</li>
+            <li><strong>Duração:</strong> {appoint.Haircut.Duracao} minutos</li>
+            <li><strong>Preço:</strong> R$ {appoint.Haircut.Preco:F2}</li>
+            <li><strong>Data e hora:</strong> {appoint.DateTime:dd/MM/yyyy HH:mm}</li>
+        </ul>
+        <p>Nos vemos em breve!</p>";
+
+            await _emailSender.SendEmailAsync(clientEmail, subjectToClient, bodyToClient);
+
 
 
 
