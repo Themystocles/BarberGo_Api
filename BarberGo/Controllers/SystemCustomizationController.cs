bf8c338bae5b6108487e73c2be7bbcd856fa02cd
Themystocles <themystocles@outlook.com>
2025-07-11 08:40:28 -0300

    Adicionando custonSystem

---
+++ BarberGo/Controllers/SystemCustomizationController.cs
@@ -0,0 +1,18 @@
+﻿using BarberGo.Entities;
+using BarberGo.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BarberGo.Controllers
+{
+    public class SystemCustomizationController : GenericRepositoryController<SystemCustomization>
+    {
+        public SystemCustomizationController(GenericRepositoryServices<SystemCustomization> genericRepositoryServices)
+             : base(genericRepositoryServices)
+        {
+          
+        }
+
+
+
+    }
+}
