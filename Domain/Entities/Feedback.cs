570e7fe5269fb15a6ce5bb6e09983e26cbc30a6e
Themystocles <themystocles@outlook.com>
2025-07-29 09:46:46 -0300

    Modificado FeedbackDto para trazer nome usuario e nome barbeiro

--- Domain/Entities/Feedback.cs
+++ Domain/Entities/Feedback.cs
@@ -6,12 +6,10 @@ namespace Domain.Entities
     public class Feedback : IEntity
     {
         public int Id { get; set; }
-        public int AppUserId { get; set; }  // Cliente
-        public int BarberId { get; set; }   // Barbeiro
-
+        public int AppUserId { get; set; }  
+        public int BarberId { get; set; }   
         public string Comment { get; set; }
         public int Rating { get; set; }
-
         public AppUser AppUser { get; set; }
         public AppUser Barber { get; set; }
 
